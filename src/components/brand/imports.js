00e1331e912cbe98850af73b584b95b6c40224e7
anobes6416 <sebonaifa777@gmail.com>
2023-11-10 07:13:06 -0500

    brand & header

---
+++ src/components/brand/imports.js
@@ -0,0 +1,13 @@
+import google from '../../assets/google.png';
+import atlassian from '../../assets/atlassian.png';
+import shopify from '../../assets/shopify.png';
+import dropbox from '../../assets/dropbox.png';
+import slack from '../../assets/slack.png';
+
+export {
+    google,
+    atlassian,
+    shopify,
+    dropbox,
+    slack,
+}
